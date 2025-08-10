7b8322a7a5dc7578691ef192a513ef898c443e70
rahulxeel <rahulxeel@xeel.xeel>
2025-08-11 03:36:55 +0530

    Just Do It

---
+++ 2__Component/src/App.jsx
@@ -0,0 +1,24 @@
+import Header from "./Header"
+import Footer from "./Footer"
+import MainBody from "./MainBody"
+
+
+function App() {
+
+  return (
+
+
+    <div>
+
+      <h1>THese Are My Components</h1>
+       <Header  />
+       <MainBody />
+       <Footer />
+
+    </div>
+
+  )
+
+}
+
+export default App
---
+++ 2__Component/src/MainBody.jsx
@@ -0,0 +1,8 @@
+function MainBody()
+{
+    return<div>
+        <h1>Main Body</h1>
+    </div>
+}
+
+export default MainBody;
