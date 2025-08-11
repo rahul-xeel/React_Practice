334ed77e83097f2d34002c5e04cd73d8a58a7f7a
rahulxeel <rahulxeel@xeel.xeel>
2025-08-12 00:47:04 +0530

    Just DO it

--- 9__Call_Back_Function/src/App.jsx
+++ 9__Call_Back_Function/src/App.jsx
@@ -9,8 +9,19 @@ function Greet(name) {
 
 function App() {
   return (
-    
-      Name_Finder(Greet)
+
+    <>
+
+      {Name_Finder(Greet)}
+
+      <h1>Callback Function:</h1>
+      <ul>
+        <li>A callback function is a function that is passed as an argument to another function and is invoked (called) when needed.</li>
+        <li>In simple terms: You pass a function as an argument to another function. The function that receives the argument will call the passed function (callback) at the appropriate time or under specific conditions.</li>
+      </ul>
+
+    </>
+
   )
 }
 
