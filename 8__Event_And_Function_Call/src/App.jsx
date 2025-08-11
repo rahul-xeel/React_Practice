dc37aa1cbbea6ed46757a00413edc16a5ee14b10
rahulxeel <rahulxeel@xeel.xeel>
2025-08-11 22:04:17 +0530

    Just DO it

---
+++ 8__Event_And_Function_Call/src/App.jsx
@@ -0,0 +1,58 @@
+
+
+function Function_Refrence() {
+  return alert("Function Refrence");
+}
+
+
+function Function_Call() {
+  return alert("Function Call");
+}
+
+
+function Arg_With_Fun_Ref(Arg) {
+  return alert(Arg);
+}
+
+
+
+
+function Arg_With_Fun_Call(Arg) {
+  return alert(Arg);
+}
+
+
+
+function App() {
+
+  return (
+    <div>
+
+      <h1>Case 1 : Function Refrence </h1>
+      <button onClick={Function_Refrence}>Function Refrence</button>
+      <br />
+      <br />
+
+      <h1>Case 2 : Function Call ( )</h1>
+      <button onClick={Function_Call()}>Function Call</button>
+      <br />
+      <br />
+
+      <h1>Case 3 : Argument With Function Refrence (Arraw function used) </h1>
+      <button onClick={() => {Arg_With_Fun_Ref("Apple is Arg")}}>Arg_With_Fun_Ref </button>
+      <br />
+      <br />
+
+       <h1>Case 4 : Argument With Function Call ( )</h1>
+      <button onClick={Arg_With_Fun_Call("Ball is arg")}>Arg_With_Fun_Ref Call</button>
+      <br />
+      <br />
+
+
+
+
+    </div>
+  )
+}
+
+export default App
