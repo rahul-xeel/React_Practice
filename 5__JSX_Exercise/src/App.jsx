3d105e6433ca7d9e7e96da7c92c93c08a76f6e8c
rahulxeel <rahulxeel@xeel.xeel>
2025-08-11 18:05:36 +0530

    Just Do It

---
+++ 5__JSX_Exercise/src/App.jsx
@@ -0,0 +1,27 @@
+
+const name = "RAHUL KUMAR";
+
+
+function App() {
+
+  return (
+   
+
+    <div>
+
+      <h1>{name} TODOS</h1>
+      <img style={{height:"450px", width:"450px"}} src="src/dog.jpg" alt="Dog" />
+      <ul>
+        <li>I have to learn About Docker</li>
+        <li>And then after i to have to learn About Azure</li>
+        <li>And Bla bla bla...</li>
+      </ul>
+      <button onClick={()=>{alert("Button Clicked")}}>Button </button>
+
+
+    </div>
+
+  )
+}
+
+export default App
