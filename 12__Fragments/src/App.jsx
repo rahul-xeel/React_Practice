23f288083ce42d34eabc1b3a67dbcfa02c4b1a33
rahulxeel <rahulxeel@xeel.xeel>
2025-08-12 03:17:00 +0530

    Just DO it

---
+++ 12__Fragments/src/App.jsx
@@ -0,0 +1,17 @@
+
+function App() {
+
+  return (
+    <>
+    
+    <h1>{'</> Or <React.Fragment>'}</h1>
+    <h1>This is my first sentence</h1>
+    <h1>This is my second sentence</h1>
+    <h1>This is my third sentence</h1>
+    <h1>{'</> Or </React.Fragment>'}</h1>
+
+   </>
+  )
+}
+
+export default App
