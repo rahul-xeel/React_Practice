fa3d8f324c9f83040b11e2be5df035b94fa888e8
rahulxeel <rahulxeel@xeel.xeel>
2025-08-13 00:55:04 +0530

    Just DO it

---
+++ 26__Pass_jsx_with_props/src/App.jsx
@@ -0,0 +1,37 @@
+
+import Wrapper from "./Wrapper"
+
+
+function App() {
+
+  return (
+
+    <>
+      
+      <Wrapper color="green">
+
+          <h1>This is my 1st element</h1>
+
+      </Wrapper>
+
+       
+      <Wrapper color="red">
+
+          <h1>This is my 2nd element</h1>
+
+      </Wrapper>
+
+       
+      <Wrapper>
+
+          <h1>This is my 3rd element</h1>
+
+      </Wrapper>
+
+    </>
+
+  
+  )
+}
+
+export default App
